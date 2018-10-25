06552585a51cc7e689d5803b1b51185787327a42
MarkovChain101 <ilyayurchenko@yahoo.com>
2018-10-24 20:54:11 -0400

    finished closest distance

--- mbta/getData.js
+++ mbta/getData.js
@@ -8,34 +8,6 @@
           mapTypeId: 'roadmap'
         });
 
-		var myLat = 0;
-		var myLng = 0;
-		var me = new google.maps.LatLng(myLat, myLng)
-				if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
-					navigator.geolocation.getCurrentPosition(function(position) {
-						myLat = position.coords.latitude;
-						myLng = position.coords.longitude;
-						me = new google.maps.LatLng(myLat, myLng);
-				// Update map and go there...
-				map.panTo(me);
-				
-				// Create a marker
-				marker = new google.maps.Marker({
-					position: me,
-					title: "Here I Am!"
-				});
-				marker.setMap(map);
-					
-				// Open info window on click of marker
-				google.maps.event.addListener(marker, 'click', function() {
-					infowindow.setContent(marker.title);
-					infowindow.open(map, marker);
-					});
-				})
-			 }
-				else {
-					 alert("Geolocation is not supported by your web browser.  What a shame!");
-				 }
 
 
         var iconBase = '';
@@ -50,72 +22,76 @@
 
         var features = [
           { 
-            position: new google.maps.LatLng( 42.352271,-71.05524200000001),
+          	station_id: 'place-sstat',
+            position: new google.maps.LatLng(42.352271,-71.05524200000001),
             type: 'station'
           }, {
+          	station_id: 'place-andrw',
             position: new google.maps.LatLng(42.330154,-71.057655),
             type: 'station'
           }, {
+          	station_id: 'place-portr',
             position: new google.maps.LatLng(42.3884, -71.11914899999999),
             type: 'station'
           }, {
+          	station_id: 'place-harsq',
             position: new google.maps.LatLng(42.373362, -71.118956),
             type: 'station'
           }, {
+          	station_id: 'place-jfk',
             position: new google.maps.LatLng(42.320685, -71.052391),
             type: 'station'
           }, {
+          	station_id: 'place-shmnl',
             position: new google.maps.LatLng(42.31129, -71.0624242),
             type: 'station'
           }, {
+          	station_id: 'place-davis',
             position: new google.maps.LatLng(42.35639457, -71.056967),
             type: 'station'
           }, {
+          	station_id: 'place-alfcl',
             position: new google.maps.LatLng(42.342622, -71.029583),
             type: 'station'
           }, {
+          	station_id: 'place-knncl',
             position: new google.maps.LatLng(42.275275,-71.06573796000001),
             type: 'station'
           }, {
+          	station_id: 'place-chmnl',
             position: new google.maps.LatLng(42.29312583, -71.121815),
             type: 'station'
           }, {
+          	station_id: 'place-dwnxg',
             position: new google.maps.LatLng(42.39674, -71.121815),
             type: 'station'
           }, {
-            position: new google.maps.LatLng(42.395428,-71.08617653),
+          	station_id: 'place-qnctr',
+            position: new google.maps.LatLng(42.395428,-71.142483),
             type: 'station'
           }, {
+          	station_id: 'place-qamnl',
           	position: new google.maps.LatLng(42.36249079,-71.08617653),
           	type: 'station'
 		  }, {
-            position: new google.maps.LatLng(42.36249079,-71.070628),
+		  	station_id: 'place-asmnl',
+            position: new google.maps.LatLng(42.361166,-71.070628),
             type: 'station'
           }, {
+          	station_id: 'place-wlsta',
             position: new google.maps.LatLng(42.355518, -71.060225),
             type: 'station'
           }, {
+          	station_id: 'place-fldcr',
             position: new google.maps.LatLng(42.251809, -71.005409),
             type: 'station'
           }, {
+          	station_id: 'place-cntsq',
             position: new google.maps.LatLng(42.233391, -71.007153),
             type: 'station'
           }, {
+          	station_id: 'place-brntn',
             position: new google.maps.LatLng(42.284652,  -71.06448899999999),
-            type: 'station'
-          }, {
-            position: new google.maps.LatLng(42.2665139, -71.0203369),
-            type: 'station'
-          }, {
-            position: new google.maps.LatLng(42.300093, -71.061667),
-            type: 'station'
-          },
-          {
-            position: new google.maps.LatLng(42.365486, -71.103802),
-            type: 'station'
-          },
-          {
-            position: new google.maps.LatLng(42.2078543, -71.0011385),
             type: 'station'
           }
         ];
@@ -146,21 +122,70 @@
           //add listener, do request, get data
         });
 
-      var smallestDistance = google.maps.geometry.spherical.computeDistanceBetween(me, features[10].position);
-      console.log(smallestDistance);
-      
-       features.forEach(function(feature) {
-        if(google.maps.geometry.spherical.computeDistanceBetween(me, feature.position) < smallestDistance){
-        	smallestDistance = google.maps.geometry.spherical.computeDistanceBetween(me, feature.position);
+        var myLat = 0;
+		var myLng = 0;
+		var me = new google.maps.LatLng(myLat, myLng)
+				if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+					navigator.geolocation.getCurrentPosition(function(position) {
+
+						myLat = position.coords.latitude;
+						myLng = position.coords.longitude;
+						me = new google.maps.LatLng(myLat, myLng);
+						console.log(me);
+    				    var smallestDistance = google.maps.geometry.spherical.computeDistanceBetween(me, features[10].position);
+      					console.log(smallestDistance);
+      					var closestFeature = features[10].station_id;
+      					var closestLatLng = features[10].position;
+       	features.forEach(function(feature) {
+       					console.log(feature.position);
+        				if(google.maps.geometry.spherical.computeDistanceBetween(me, feature.position) < smallestDistance){
+        					smallestDistance = google.maps.geometry.spherical.computeDistanceBetween(me, feature.position);
+        					closestFeature = feature.station_id;
+        					closestLatLng = feature.position;
         }
-          
+		var closePos = new google.maps.MVCArray([
+        closestLatLng, 
+		me
+    ]);
+
+        console.log(closePos);
+          var closeLine = new google.maps.Polyline({
+          path: closePos,
+          geodesic: true,
+          strokeColor: '#0000FF',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+
+        closeLine.setMap(map)
+    });
+				// Update map and go there...
+				map.panTo(me);
+				console.log(smallestDistance);		
+				// Create a marker
+				marker = new google.maps.Marker({
+					position: me,
+					title: "Here I Am!"
+				});
+				marker.setMap(map);
+					
+				// Open info window on click of marker
+				google.maps.event.addListener(marker, 'click', function() {
+					infowindow.setContent(marker.title);
+					infowindow.open(map, marker);
+					});
+				}
+			 
+			)}	else {
+					 alert("Geolocation is not supported by your web browser.  What a shame!");
+				 }
  
           //marker.addListener("click", getData("place-sstat"));
-          console.log("ooo");
+    
           //add listener, do request, get data
-        });
-			console.log(smallestDistance);
-}
+        };
+
+
 
 
 
